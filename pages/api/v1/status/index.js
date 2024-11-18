18424e767a217a4bb0b5d994c6e950c187f6a0bd
g-bolsoni <giovanebolsoni2@gmail.com>
2024-11-17 23:47:05 -0300

    add ´database.js´ and ´.env´ files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
+import database from "../../../../infra/database.js";
+
 const status = async (req, res) => {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+
   res.status(200).json({ message: "Test api" });
 };
 
