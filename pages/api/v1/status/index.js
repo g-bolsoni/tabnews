99b82cd13ce1fcd2b809b63bf0211de52c7980e3
g-bolsoni <giovanebolsoni2@gmail.com>
2024-11-18 22:18:19 -0300

    feat: Finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,36 @@
 import database from "infra/database.js";
 
 const status = async (req, res) => {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  res.status(200).json({ message: "Test api" });
+  const versionDatabase = await database.query("SHOW server_version;");
+  const versionDatabaseValue = versionDatabase.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+
+  const DATABASE_NAME = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [DATABASE_NAME]
+  });
+
+  const databaseOpenedConnectionsValue = databaseOpenedConnections.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versionDatabaseValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        open_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 };
 
 export default status;
