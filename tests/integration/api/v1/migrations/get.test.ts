9af1da98c0cefc91f434c1285a7e1632c51a16dc
g-bolsoni <giovanebolsoni2@gmail.com>
2024-11-23 19:43:23 -0300

    feat: make `npm test` more robust with `async-retry` and `orchestrator.ts`

--- tests/integration/api/v1/migrations/get.test.ts
+++ tests/integration/api/v1/migrations/get.test.ts
@@ -1,10 +1,9 @@
 import database from "infra/database";
-
-const cleanDatabase = async () => {
+import orchestrator from "tests/orchestrator";
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-};
-
-beforeAll(cleanDatabase);
+});
 
 test("Get to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
