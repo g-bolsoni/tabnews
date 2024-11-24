0294a6631b0dbddaed79b698f76601cdb764265b
g-bolsoni <giovanebolsoni2@gmail.com>
2024-11-24 10:55:27 -0300

    fix: Fix bug

--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -7,7 +7,7 @@ beforeAll(async () => {
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
-  expect(response.status).toBe(300);
+  expect(response.status).toBe(200);
   const responseBody = await response.json();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
