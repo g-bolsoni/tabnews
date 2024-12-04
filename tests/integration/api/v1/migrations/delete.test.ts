ffef50f7f5bbd28ba98749b003894111c1ad5f7b
g-bolsoni <giovanebolsoni2@gmail.com>
2024-12-03 23:41:33 -0300

    test: rename all descriptions to use new pattern

---
+++ tests/integration/api/v1/migrations/delete.test.ts
@@ -0,0 +1,29 @@
+import database from "infra/database";
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await database.query("drop schema public cascade; create schema public;");
+});
+
+describe("DELETE /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Should keep open_connections as 1", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "DELETE",
+      });
+
+      expect(response.status).toBe(405);
+
+      const responseStatusConnections = await fetch(
+        "http://localhost:3000/api/v1/status",
+      );
+
+      const connectionsBody = await responseStatusConnections.json();
+      const openConnections =
+        connectionsBody.dependencies.database.open_connections;
+
+      expect(openConnections).toBeLessThanOrEqual(1);
+    });
+  });
+});
