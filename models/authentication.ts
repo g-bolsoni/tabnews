b02ceb55b3b4015c9785d185b002bb2c10897903
giovane.souza <giovane.essado@gmail.com>
2026-05-13 23:33:17 -0300

    refactor: ajust error messages

--- models/authentication.ts
+++ models/authentication.ts
@@ -1,36 +1,37 @@
 import { NotFoundError, UnauthorizedError } from "infra/error";
 import password from "./password";
 import user from "./user";
+import { User } from "interfaces/InterfaceTables";
 
-const getAuthenticatedUser = async (email: string, providePassword: string) => {
+const defaultErrorMessages = {
+  message: "Dados de autenticação não conferem.",
+  action: "Verifique os dados informados e tente novamente.",
+}
+
+
+const getAuthenticatedUser = async (email: string, providePassword: string): Promise<User> => {
   try {
     const storedUser = await findUserByEmail(email);
     await validatePassword(providePassword, storedUser.password);
-    return storedUser;
+
+    return storedUser
+
   } catch (error) {
-    if (error instanceof UnauthorizedError) {
-      throw new UnauthorizedError({
-        message: "Dados de autenticação não conferem.",
-        action: "Verifique os dados informados e tente novamente.",
-      });
-    }
+    if (error instanceof UnauthorizedError)
+      throw new UnauthorizedError(defaultErrorMessages);
 
     throw error;
   }
 };
 
 const findUserByEmail = async (email: string) => {
-  let storagedUser: any;
+  let storagedUser: User;
 
   try {
     storagedUser = await user.findOneByEmail(email);
   } catch (error) {
-    if (error instanceof NotFoundError) {
-      throw new UnauthorizedError({
-        message: "Dados de autenticação não conferem.",
-        action: "Verifique os dados informados e tente novamente.",
-      });
-    }
+    if (error instanceof NotFoundError)
+      throw new UnauthorizedError(defaultErrorMessages);
 
     throw error;
   }
@@ -38,24 +39,20 @@ const findUserByEmail = async (email: string) => {
   return storagedUser;
 };
 
-const validatePassword = async (
-  providedPassword: string,
-  storedPassword: string,
-) => {
+const validatePassword = async ( providedPassword: string, storedPassword: string ) => {
   const isPasswordValid = await password.compare(
     providedPassword,
     storedPassword,
   );
-  if (!isPasswordValid) {
-    throw new UnauthorizedError({
-      message: "Dados de autenticação não conferem.",
-      action: "Verifique os dados informados e tente novamente.",
-    });
-  }
+
+  if (!isPasswordValid)
+    throw new UnauthorizedError(defaultErrorMessages);
+
   return isPasswordValid;
 };
 
 const authentication = {
-  getAuthenticatedUser,
+  getAuthenticatedUser
 };
+
 export default authentication;
