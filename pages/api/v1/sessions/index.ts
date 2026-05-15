3793446038390ee09cdd3fcc95121be9fe64099f
giovane.souza <giovane.essado@gmail.com>
2026-05-14 21:50:27 -0300

    chore: implements cookie for navigator

--- pages/api/v1/sessions/index.ts
+++ pages/api/v1/sessions/index.ts
@@ -1,19 +1,30 @@
+import * as cookie from 'cookie'
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
 import authentication from "models/authentication";
 import session from "models/session";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { EXPIRATION_IN_MILLISECONDS } from '../../../../constants';
 
 const router = createRouter();
-
 router.post(postHandler);
 
 export default router.handler(controller.onErrorHandlers);
 
-async function postHandler(req, res) {
+async function postHandler(req:NextApiRequest, res: NextApiResponse) {
+
   const userInputValues = req.body;
 
   const { id } = await authentication.getAuthenticatedUser(userInputValues.email,userInputValues.password);
   const userSession = await session.create(id);
 
+  const setCookie = cookie.serialize('session_id', userSession.token,{
+    path: '/',
+    maxAge: EXPIRATION_IN_MILLISECONDS / 1000,
+    secure: process.env.NODE_ENV === 'production' ? true : false,
+    httpOnly: true
+  })
+
+  res.setHeader("Set-Cookie",setCookie )
   return res.status(201).json(userSession);
 }
